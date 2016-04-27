7a3e6a1ecd89e93769763bc1027c4717181da7fa
Anna <ania.buraczek@gmail.com>
2016-04-27 20:15:34 +0200

    source

--- src/InOut2.java
+++ src/InOut2.java
@@ -1,11 +1,14 @@
-
+import java.util.Scanner;
 public class InOut2 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = Integer.parseInt(sc.nextLine());
+        double y = Double.parseDouble(sc.nextLine());
+        String s = sc.nextLine();
 
+        System.out.println("String: "+s);
+        System.out.println("Double: "+y);
+        System.out.println("Int: "+x);
 
     }
-
-
-
-
 }
