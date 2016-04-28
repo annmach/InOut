14408c19d3d563c189af0de0490891c78a14b96b
Anna <ania.buraczek@gmail.com>
2016-04-28 18:12:03 +0200

    save

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,6 +1,14 @@
+import java.util.*;
 
 public class IfElse {
-
+    public static void main(String[] args){
+        System.out.println("podaj liczbe");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("podana liczba jest parzysta");
+        }else{System.out.println("podana liczba jest nieparzysta");}
+    }
 
 
 }
